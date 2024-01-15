9e0764da25d7945588ea3e5dae71bcb87f9bba73
C5D2 <yoakec5d2@naver.com>
2024-01-15 21:41:44 +0900

    1주차 스터디 내용 정리

--- JavaScript/Week1/method.js
+++ JavaScript/Week1/method.js
@@ -17,9 +17,126 @@ const superman = {
   },
 };
 
-const user = {
+// const user = {
+//   name: "Mike",
+//   sayHello: function () {
+//     console.log(`Hello, I'm`${user.name}`);
+//   },
+// };
+// 문제 발생할 수 있음 this를 이용해서
+
+// const user = {
+//     name: "Mike",
+//     sayHello: function () {
+//       console.log(`Hello, I'm`${this.name}`);
+//     },
+//   };
+//   하자!
+
+let boy = {
   name: "Mike",
-  sayHello: function () {
-    console.log(`Hello, I'm`${user.name}`);
-  },
 };
+let girl = {
+  name: "Jane",
+};
+
+sayHello : function(){
+    console.log(`Hello, I'm ${this.name}`);
+}
+// 결과
+boy.sayHello();//I'm Mike
+girl.sayHello();//I'm Jane
+
+//만약 화살표 함수를 사용했다면?
+sayHello : () => {
+    console.log(`Hello, I'm ${this.name}`);}
+
+// 화살표 함수는 일반 함수와는 달리 자신만의 this를 가지지 않음
+// 화살표 함수 내부에서 this를 사용하면, 그 this는 외부에서 값을 가져옴
+
+let boy = {
+    name: "Mike",
+    sayHello: () => {
+      console.log(this); // 전역객체 브라우저환경에선 window, 노드 js에선 global
+    },  
+}
+
+boy.sayHello();
+this != boy
+
+// method
+
+let boy = {
+    name: "Mike",
+    showName: function () {
+        console.log(boy.name)
+    } 
+};
+ // "Mike" 출력
+
+
+let man = boy;
+man.name = "Tom"
+
+console.log(boy.name); 
+// "Tom" 출력
+
+let boy = {
+    name: "Mike",
+    showName: function () {
+        console.log(boy.name)
+    } 
+};
+
+let man = boy;
+man.showName();
+// "Mike" 출력
+
+let boy = {
+    name: "Mike",
+    showName: function () {
+        console.log(boy.name)
+    } 
+};
+
+let man = boy;
+boy = null;
+
+man.showName();
+// boy는 null, name, showName은 다 사라져서 동작 불가
+
+let boy = {
+    name: "Mike",
+    showName: function () {
+        console.log(this.name)
+    } 
+};
+
+let man = boy;
+boy = null;
+
+man.showName();
+// "Mike" 출력
+
+let boy = {
+    name: "Mike",
+    sayThis : function() {
+        console.log(this)
+    },
+};
+
+boy.sayThis();
+// Object {
+//   "name": "Mike",
+//   "sayThis": function () {\n    console.log(this);\n  }
+// } 출력
+
+let boy = {
+    name: "Mike",
+    sayThis : () => {
+        console.log(this)
+    },
+};
+
+boy.sayThis();
+// window 출력
---
+++ JavaScript/array.js
@@ -0,0 +1,95 @@
+// 배열: 순서가 있는 리스트
+// 배열은 문자뿐만 아니라, 숫자, 객체, 함수 등도 포함할 수 있음
+
+let arr = [
+  "민수",
+  3,
+  false,
+  {
+    name: "Mike",
+    age: 30,
+  },
+  function () {
+    console.log("TEST");
+  },
+];
+
+// length: 배열의 길이
+students.length; // 30
+
+// push(): 배열 끝에 추가
+let days = ["월", "화", "수"];
+days.push("목");
+console.log(days); // ["월", "화", "수", "목"]
+
+// pop(): 배열 끝 요소 삭제
+let days = ["월", "화", "수"];
+days.pop();
+console.log(days); //["월", "화"]
+
+//shift, unshift 배열 앞에 제거/추가
+// 추가
+days.unshift("일");
+console.log(days); // ["일", "월", "화", "수"];
+
+// 제거
+days.shift();
+console.log(days); // ["월", "화", "수"];
+
+// 반복문:for
+let days = ["월", "화", "수"];
+
+for (let index = 0; index < days.length; index++) {
+  console.log(days[index]);
+}
+// "월" "화" "수" 돌아가면서 출력
+
+// 반복문: for ... of
+let days = ["월", "화", "수"];
+
+for (let day of days) {
+  console.log(day);
+}
+// "월" "화" "수" 돌아가면서 출력, for문 보다는 간단하지만 index를 못 얻음
+
+// array
+let days = ["mon", "tue", "wed"];
+console.log(days[1]);
+// "tue" 출력
+
+let days = ["mon", "tue", "wed"];
+days[1] = "화요일";
+console.log(days);
+// ["mon", "화요일", "wed"] 출력
+
+let days = ["mon", "tue", "wed"];
+days[1] = "화요일";
+console.log(days.length);
+// 3 출력
+
+let days = ["mon", "tue", "wed"];
+days.push("thu");
+console.log(days);
+// ["mon", "tue", "wed", "thu"] 출력
+
+let days = ["mon", "tue", "wed"];
+days.push("thu");
+days.unshift("sun");
+console.log(days);
+// ["sun", "mon", "tue", "wed", "thu"] 출력
+
+let days = ["mon", "tue", "wed"];
+days.push("thu");
+days.unshift("sun");
+for (let index = 0; index < days.length; index++) {
+  console.log(days[index]);
+}
+// "sun" "mon" "tue" "wed" "thu" 출력
+
+let days = ["mon", "tue", "wed"];
+days.push("thu");
+days.unshift("sun");
+for (let day of days) {
+  console.log(day);
+}
+// "sun" "mon" "tue" "wed" "thu" 출력
